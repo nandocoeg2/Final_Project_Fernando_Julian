037d173fa8a4635f24d415f02fce13ed417bc99f
Fernando Julian <nandokun69@gmail.com>
2023-06-15 23:47:31 +0700

    add r2d to routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/login" element={Login()} />
+        <Route path="/register" element={<div>Register</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
