91d6eeee2b2ec6d772570d5bbbb3701825dee26c
Fernando Julian <nandokun69@gmail.com>
2023-07-02 14:24:39 +0700

    add button add operator, edit, delete operator

--- frontend/src/Pages/Operator.jsx
+++ frontend/src/Pages/Operator.jsx
@@ -1,8 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navigation from "../components/Molecules/Navigation";
 import Header from "../components/Molecules/Header";
+import { useRefreshTokenMutation } from "../features/users";
+import jwt_decode from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+import { axiosInstance } from "../app/axios";
+import axios from "axios";
 
 export const Operator = () => {
+  const [name, setName] = useState("");
+  const [token, setToken] = useState("");
+  const [expire, setExpire] = useState("");
+  const [role, setRole] = useState("");
+  const [users, setUsers] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    responseToken();
+    getUsers();
+  }, []);
+
+  const [refreshToken] = useRefreshTokenMutation();
+
+  const responseToken = async () => {
+    try {
+      const response = await refreshToken();
+      setToken(response.data.accessToken);
+      const decoded = jwt_decode(response.data.accessToken);
+      setName(decoded.name);
+      setExpire(decoded.exp);
+      setRole(decoded.role);
+    } catch (error) {
+      navigate("/login");
+    }
+  };
+
+  axiosInstance.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (expire * 1000 < currentDate.getTime()) {
+        const response = await axios.get("http://localhost:2000/token");
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+        const decoded = jwt_decode(response.data.accessToken);
+        setName(decoded.name);
+        setExpire(decoded.exp);
+        setRole(decoded.role);
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    }
+  );
+
+  const getUsers = async () => {
+    const response = await axiosInstance.get("http://localhost:2000/users", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    setUsers(response.data);
+  };
+
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
@@ -16,40 +76,36 @@ export const Operator = () => {
         {/* Konten Dashboard */}
         <div className="flex-1 bg-white p-8">
           <div className="container p-4 mt-md-4 mt-2 border">
-            <h2 className="text-2xl font-semibold mb-6">Profiles</h2>
+            <div className="flex justify-between">
+              <h2 className="text-2xl font-semibold mb-6">Operator</h2>
+              <button className="btn btn-md btn-active">Add Operator</button>
+            </div>
             <div className="overflow-x-auto">
               <table className="table">
                 {/* head */}
                 <thead>
                   <tr>
-                    <th></th>
+                    <th>No</th>
                     <th>Name</th>
-                    <th>Job</th>
-                    <th>Favorite Color</th>
+                    <th>Email</th>
+                    <th>Role</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
+                {/* body */}
                 <tbody>
-                  {/* row 1 */}
-                  <tr>
-                    <th>1</th>
-                    <td>Cy Ganderton</td>
-                    <td>Quality Control Specialist</td>
-                    <td>Blue</td>
-                  </tr>
-                  {/* row 2 */}
-                  <tr>
-                    <th>2</th>
-                    <td>Hart Hagerty</td>
-                    <td>Desktop Support Technician</td>
-                    <td>Purple</td>
-                  </tr>
-                  {/* row 3 */}
-                  <tr>
-                    <th>3</th>
-                    <td>Brice Swyre</td>
-                    <td>Tax Accountant</td>
-                    <td>Red</td>
-                  </tr>
+                  {users.map((user, index) => (
+                    <tr key={user.id}>
+                      <td>{index + 1}</td>
+                      <td>{user.name}</td>
+                      <td>{user.email}</td>
+                      <td>{user.role.name}</td>
+                      <td className="flex gap-3">
+                        <button className="btn btn-sm btn-active">Edit</button>
+                        <button className="btn btn-sm btn-error">Delete</button>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
@@ -59,4 +115,3 @@ export const Operator = () => {
     </div>
   );
 };
-
