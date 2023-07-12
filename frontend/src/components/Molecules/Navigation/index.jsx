6808ea22e16f6d5c12fd7854f72360b18b2a2db6
Fernando Julian <nandokun69@gmail.com>
2023-07-12 18:12:13 +0700

    fix navigation menu map

--- frontend/src/components/Molecules/Navigation/index.jsx
+++ frontend/src/components/Molecules/Navigation/index.jsx
@@ -9,28 +9,20 @@ const Navigation = () => {
   }, []);
 
   const [getMenu] = useMenuMutation();
-  const [openSubMenus, setOpenSubMenus] = useState([]);
 
   const responseMenu = async () => {
     try {
       const response = await getMenu();
-      setMenu(response.data.menus);
+      setMenu(response.data.menus); // Update to response.data.menus
+      console.log(response.data.menus);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const toggleSubMenu = (itemId) => {
-    if (openSubMenus.includes(itemId)) {
-      setOpenSubMenus(openSubMenus.filter((id) => id !== itemId));
-    } else {
-      setOpenSubMenus([...openSubMenus, itemId]);
-    }
-  };
-
   return (
     <div className="h-full flex overflow-hidden bg-gray-100">
-      <div className="h-full drawer lg:drawer-open">
+      <div className="drawer lg:drawer-open">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-center">
           {/* Page content here */}
@@ -38,53 +30,17 @@ const Navigation = () => {
             htmlFor="my-drawer-2"
             className="btn btn-primary drawer-button lg:hidden"
           >
-            Open Menu
+            Open drawer
           </label>
         </div>
         <div className="drawer-side">
           <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-          <ul className="menu p-4 w-64 h-full bg-base-200 text-base-content">
+          <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
             {/* Sidebar content here */}
             {menu.map((item) => (
-              <div key={item.id}>
-                {item.subMenus.length > 0 ? (
-                  <>
-                    <a
-                      href={item.url}
-                      className={`flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-gray-200 hover:text-gray-900 ${
-                        openSubMenus.includes(item.id)
-                          ? "bg-gray-200 text-gray-900"
-                          : ""
-                      }`}
-                      onClick={() => toggleSubMenu(item.id)}
-                    >
-                      {item.name}
-                    </a>
-                    <div
-                      className={`ml-6 ${
-                        openSubMenus.includes(item.id) ? "block" : "hidden"
-                      }`}
-                    >
-                      {item.subMenus.map((subMenu) => (
-                        <a
-                          key={subMenu.id}
-                          href={subMenu.url}
-                          className="flex items-center px-6 py-2 text-sm text-gray-500 hover:text-gray-900"
-                        >
-                          {subMenu.name}
-                        </a>
-                      ))}
-                    </div>
-                  </>
-                ) : (
-                  <a
-                    href={item.url}
-                    className="flex items-center px-3 py-2 text-sm font-medium text-gray-600 hover:bg-gray-200 hover:text-gray-900"
-                  >
-                    {item.name}
-                  </a>
-                )}
-              </div>
+              <li key={item.id}>
+                <a href={item.url}>{item.name}</a>
+              </li>
             ))}
           </ul>
         </div>
